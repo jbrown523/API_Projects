09ac3dc76a7e308f161482ba114f9c2ab0a21e99
Jalen Brown <jalenbrown523@gmail.com>
2022-07-03 10:33:44 -0500

    Created default users

--- Routes/user.js
+++ Routes/user.js
@@ -3,10 +3,30 @@ import express from 'express';
 // Initialize router
 const router = express.Router();
 
+const user = [
+
+    {
+        firstName: "Jalen",
+        lastName: "B",
+        age: 21
+    },
+    {
+        firstName: "kane",
+        lastName: "ke",
+        age: 22
+    }
+    
+]
+
+
+
+
+
 // Initialize routes to /user
 // All routes start with /user
 router.get('/', (req, res) => {
-    res.send('Welcome');
+    console.log(user);
+    res.send(user);
 });
 
 export default router;
