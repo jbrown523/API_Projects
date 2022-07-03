c8c0d845bc0de272cef48caf1807ac823d1da215
Jalen Brown <jalenbrown523@gmail.com>
2022-07-03 11:08:16 -0500

    Postman Adding a user: push method

--- Routes/user.js
+++ Routes/user.js
@@ -19,14 +19,30 @@ const user = [
 ]
 
 
-
-
-
 // Initialize routes to /user
 // All routes start with /user
 router.get('/', (req, res) => {
     console.log(user);
+    // res.send - Output to client
     res.send(user);
+});
+
+// Send data frorm client to server
+// Cannot use browser to test post request, browsers only make GET requests 
+// 
+router.post('/', (req, res) => {
+    console.log("POSTMAN REACHED");
+
+    // POST request have req.body
+    console.log(req.body);
+
+    const newUser = req.body
+
+    // Add values to user array
+    user.push(newUser);
+
+    // Output to client
+    res.send(`User with name ${newUser.name} added to the database.`); // See output in browser
 });
 
 export default router;
