e027b4a977bb213b43d317c148c73a3f296f67b2
Jalen Brown <jalenbrown523@gmail.com>
2022-07-03 19:03:21 -0500

    Delete User by id

--- Routes/user.js
+++ Routes/user.js
@@ -7,10 +7,12 @@ uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 // Initialize router
 const router = express.Router();
 // Array of users
-const user = []
+//const user = []
+// User array changes
+let user = []
 
 
-// Initialize routes to /user.
+// Initialize routes to /user
 // All routes start with /user
 // Get users in database
 router.get('/', (req, res) => {
@@ -20,7 +22,7 @@ router.get('/', (req, res) => {
 
 // Send  data frorm client to server
 // Cannot use browser to test post request, browsers only make GET requests 
-// Add user to database
+// Add user to database**
 router.post('/', (req, res) => {
     // POST request have req.body - info about the request
     // {firstName: 'Jalen', lastName: 'B', age: 21 }
@@ -47,9 +49,26 @@ router.get('/:id', (req, res) => {
     const {id} = req.params.id;
 
     // Find user with the same id
-   const findUser = user.find(user => user.id === id);
+    const findUser = user.find(user => user.id === id);
 
    res.send(findUser);
+});
+
+// Delete User** 
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    // Id to delete 123
+    // Jalen 123, Kaene 321
+    // 1st ineteration is Jalen, since the Id to delete is is 123, Jalen is deleted
+    // 2nd ineteration is Kane, Id to delete is 123, (Kane user.id !== id), Kane not deleted
+
+    // B/c !==, if the user.id = id, then delete
+    // Returns false b/c the id is equal, so Jalen is deleted
+    // Kane is not equal, returns true, keep Kane
+    user = user.filter(user => user.id !== id);
+
+    res.send(`User with the id ${user.id} is deleted`);
 });
 
 export default router;
